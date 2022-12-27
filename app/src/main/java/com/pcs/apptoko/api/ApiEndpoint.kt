71a270b56d61b198936e2944c7e3ee453bc340b2
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2022-12-27 17:40:17 +0700

    update code pertemuan 12

--- app/src/main/java/com/pcs/apptoko/api/ApiEndpoint.kt
+++ app/src/main/java/com/pcs/apptoko/api/ApiEndpoint.kt
@@ -11,4 +11,56 @@ interface ApiEndpoint {
         @Field("email") email : String,
         @Field("password") password : String
     ) : Call<LoginResponse>
+
+    @GET("produk")
+    fun getProduk(@Header("Authorization") token : String) : Call<ProdukResponse>
+
+    @FormUrlEncoded
+    @POST("produk")
+    fun postProduk(
+        @Header("Authorization") token: String,
+        @Field("admin_id") admin_id : Int,
+        @Field("nama") nama : String,
+        @Field("harga") harga : Int,
+        @Field("strok") strok : Int
+    ) : Call<ProdukResponsePost>
+
+    @FormUrlEncoded
+    @HTTP(method = "DELETE", path = "produk", hasBody = true)
+    fun deleteProduk(
+        @Header("Authorization") token : String,
+        @Field("id") id : Int
+    ) : Call<ProdukResponsePost>
+
+    @FormUrlEncoded
+    @HTTP(method = "PUT", path = "produk", hasBody = true)
+    fun putProduk(
+        @Header("Authorization") token : String,
+        @Field("id") id : Int,
+        @Field("admin_id") admin_id : Int,
+        @Field("nama") nama : String,
+        @Field("harga") harga : Int,
+        @Field("stok") stok : Int,
+    ) : Call<ProdukResponsePost>
+
+    @FormUrlEncoded
+    @POST("transaksi")
+    fun postTransaksi(
+        @Header("Authorization") token : String,
+        @Field("admin_id") admin_id : Int,
+        @Field("total") total : Int
+    ) : Call<TransaksiResponsePost>
+
+    @FormUrlEncoded
+    @POST("item_transaksi")
+    fun postItemTransaski(
+        @Header("Authorization") token : String,
+        @Field("transasi_id") transaksi_id : Int,
+        @Field("produk_id") produk_id : Int,
+        @Field("qty") qty : Int,
+        @Field("harga_saat_transaksi") harga_saat_transaksi : Int
+    ) : Call<ItemTransasiResponsePost>
+
+    @GET("transasi_bulan_ini")
+    fun getTransaksi(@Header("Authorization") token : String) : Call<TransaksiResponse>
 }
